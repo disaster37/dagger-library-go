9f09bcd3693de183c37e361a29d3663e35d09233
langoureaux-s <Sebastien.LANGOUREAUX@harmonie-mutuelle.fr>
2023-12-07 14:23:56 +0000

    feat: Add readme generator

--- helm/generate.go
+++ helm/generate.go
@@ -2,6 +2,7 @@ package helm
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"dagger.io/dagger"
@@ -15,6 +16,7 @@ import (
 type HelmGenerateOption struct {
 	WithProxy   bool   `default:"true"`
 	PathContext string `default:"."`
+	FileName    string `validate:"required"`
 }
 
 // GetBuildCommand permit to get the command spec to add on cli
@@ -29,6 +31,11 @@ func GetGenerateSchemaCommand() *cli.Command {
 				Value:   ".",
 				EnvVars: []string{"REGISTRY_CERT_PATH"},
 			},
+			&cli.StringFlag{
+				Name:  "schema-file",
+				Usage: "The schema file name",
+				Value: "values.schema.json",
+			},
 		},
 		Action: func(c *cli.Context) (err error) {
 			// initialize Dagger client
@@ -40,6 +47,7 @@ func GetGenerateSchemaCommand() *cli.Command {
 
 			generateOption := &HelmGenerateOption{
 				PathContext: c.String("path"),
+				FileName:    c.String("schema-file"),
 			}
 
 			return GenerateHelmSchema(c.Context, client, generateOption)
@@ -58,19 +66,8 @@ func GenerateHelmSchema(ctx context.Context, client *dagger.Client, option *Helm
 		panic(err)
 	}
 
-	container := client.
-		Container().
-		From("node:21-alpine")
-
-	if option.WithProxy {
-		container = helper.WithProxy(container)
-	}
-
-	_, err = container.
-		WithDirectory("/project", client.Host().Directory(option.PathContext)).
-		WithWorkdir("/project").
-		WithExec(helper.ForgeCommand("npm install -g @bitnami/readme-generator-for-helm")).
-		WithExec(helper.ForgeCommand("readme-generator -s values.schema.json --values values.yaml")).
+	getGeneratorContainer(client, option).
+		WithExec(helper.ForgeCommand(fmt.Sprintf("readme-generator -s %s --values values.yaml", option.FileName))).
 		Directory(".").
 		Export(ctx, option.PathContext)
 
@@ -79,4 +76,78 @@ func GenerateHelmSchema(ctx context.Context, client *dagger.Client, option *Helm
 	}
 
 	return nil
+}
+
+// GetBuildCommand permit to get the command spec to add on cli
+func GetGenerateReadmeCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "generateHelmSchema",
+		Usage: "Generate the helm schema",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "path",
+				Usage:   "The path of helm chart",
+				Value:   ".",
+				EnvVars: []string{"REGISTRY_CERT_PATH"},
+			},
+			&cli.StringFlag{
+				Name:  "readme-file",
+				Usage: "The readme file name to generate",
+				Value: "schema.md",
+			},
+		},
+		Action: func(c *cli.Context) (err error) {
+			// initialize Dagger client
+			client, err := helper.WithCustomCa(c.Context, c.String("registry-cert-path"), dagger.WithLogOutput(os.Stdout))
+			if err != nil {
+				panic(err)
+			}
+			defer client.Close()
+
+			generateOption := &HelmGenerateOption{
+				PathContext: c.String("path"),
+				FileName:    c.String("readme-file"),
+			}
+
+			return GenerateHelmReadme(c.Context, client, generateOption)
+		},
+	}
+}
+
+// BuildHelm permit to build helm chart
+func GenerateHelmReadme(ctx context.Context, client *dagger.Client, option *HelmGenerateOption) (err error) {
+
+	if err = defaults.Set(option); err != nil {
+		panic(err)
+	}
+
+	if err = validate.Struct(option).ValidateErr(); err != nil {
+		panic(err)
+	}
+
+	getGeneratorContainer(client, option).
+		WithExec(helper.ForgeCommand(fmt.Sprintf("readme-generator -r %s --values values.yaml", option.FileName))).
+		Directory(".").
+		Export(ctx, option.PathContext)
+
+	if err != nil {
+		return errors.Wrap(err, "Error when generate helm readme")
+	}
+
+	return nil
+}
+
+func getGeneratorContainer(client *dagger.Client, option *HelmGenerateOption) *dagger.Container {
+	container := client.
+		Container().
+		From("node:21-alpine")
+
+	if option.WithProxy {
+		container = helper.WithProxy(container)
+	}
+
+	return container.
+		WithDirectory("/project", client.Host().Directory(option.PathContext)).
+		WithWorkdir("/project").
+		WithExec(helper.ForgeCommand("npm install -g @bitnami/readme-generator-for-helm"))
 }
