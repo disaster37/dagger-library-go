23a24593e9ffeed9a0f0e72464aeb6bc676013c7
disaster37 <linuxworkgroup@hotmail.com>
2023-12-21 15:58:24 +0000

    fix: git push
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- git/push.go
+++ git/push.go
@@ -2,21 +2,35 @@ package git
 
 import (
 	"context"
+	"fmt"
 
 	"dagger.io/dagger"
+	"github.com/creasty/defaults"
+	"github.com/gookit/validate"
 )
 
-func CommitAndPush(ctx context.Context, client *dagger.Client, path string) (err error) {
+type GitOption struct {
+	PathContext string `default:"."`
+	Author      string `validate:"required"`
+	Email       string `validate:"required"`
+}
 
-	_, err = getGitContainer(client, path).
+func CommitAndPush(ctx context.Context, client *dagger.Client, option *GitOption) (err error) {
+	if err = defaults.Set(option); err != nil {
+		panic(err)
+	}
+
+	if err = validate.Struct(option).ValidateErr(); err != nil {
+		panic(err)
+	}
+
+	_, err = getGitContainer(client, option.PathContext).
 		WithEntrypoint([]string{"/bin/sh", "-c"}).
-		WithExec([]string{`
-set -e
-
-git add -A
-git commit -m "push back from pipeline"
-git push
-		`}).
+		WithExec([]string{fmt.Sprintf("git config --global user.email %s", option.Email)}).
+		WithExec([]string{fmt.Sprintf("git config --global user.name %s", option.Author)}).
+		WithExec([]string{"git add -A "}).
+		WithExec([]string{"git commit -m \"push back from pipeline\""}).
+		WithExec([]string{"git push"}).
 		Stdout(ctx)
 
 	return err
