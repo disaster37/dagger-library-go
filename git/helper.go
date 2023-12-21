318fa44d2125f3dab8d08c8895c6898cfcf90de4
disaster37 <linuxworkgroup@hotmail.com>
2023-12-21 16:15:55 +0000

    fix: fix git
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- git/helper.go
+++ git/helper.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 
 	"dagger.io/dagger"
+	"github.com/disaster37/dagger-library-go/helper"
 )
 
 const (
 	git_version string = "2.43.0"
 )
 
-func getGitContainer(client *dagger.Client, path string) *dagger.Container {
+func getGitContainer(client *dagger.Client, path string, withProxy bool) *dagger.Container {
 	image := fmt.Sprintf("bitnami/git:%s", git_version)
 
-	return client.
+	container := client.
 		Container().
 		From(image).
 		WithDirectory("/project", client.Host().Directory(path, dagger.HostDirectoryOpts{Exclude: []string{"ci"}})).
 		WithWorkdir("/project")
+
+	if withProxy {
+		container = helper.WithProxy(container)
+	}
+
+	return container
 }
--- git/push.go
+++ git/push.go
@@ -7,12 +7,28 @@ import (
 	"dagger.io/dagger"
 	"github.com/creasty/defaults"
 	"github.com/gookit/validate"
+	"github.com/urfave/cli/v2"
 )
 
 type GitOption struct {
 	PathContext string `default:"."`
 	Author      string `validate:"required"`
 	Email       string `validate:"required"`
+	Token       string `validate:"required"`
+	WithProxy   bool   `default:"true"`
+}
+
+func InitGitFlag(app *cli.App) {
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:     "git-token",
+			Usage:    "The git token",
+			Required: false,
+			EnvVars:  []string{"GIT_TOKEN"},
+		},
+	}
+
+	app.Flags = append(app.Flags, flags...)
 }
 
 func CommitAndPush(ctx context.Context, client *dagger.Client, option *GitOption) (err error) {
@@ -24,10 +40,12 @@ func CommitAndPush(ctx context.Context, client *dagger.Client, option *GitOption
 		panic(err)
 	}
 
-	_, err = getGitContainer(client, option.PathContext).
+	_, err = getGitContainer(client, option.PathContext, option.WithProxy).
 		WithEntrypoint([]string{"/bin/sh", "-c"}).
 		WithExec([]string{fmt.Sprintf("git config --global user.email %s", option.Email)}).
 		WithExec([]string{fmt.Sprintf("git config --global user.name %s", option.Author)}).
+		WithExec([]string{fmt.Sprintf("git remote set-url origin https://%s@$(git config remote.origin.url)", option.Token)}).
+		WithExec([]string{"git checkout master"}).
 		WithExec([]string{"git add -A "}).
 		WithExec([]string{"git commit -m \"push back from pipeline\""}).
 		WithExec([]string{"git push"}).
