7c8c87de911558eb9546faa7d7b71f72a6e28aa4
disaster37 <linuxworkgroup@hotmail.com>
2024-04-08 13:55:17 +0000

    fix: fix helm build. Run helm repo update before if dependency.
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- helm/build.go
+++ helm/build.go
@@ -136,6 +136,7 @@ func BuildHelm(ctx context.Context, client *dagger.Client, option *BuildOption) 
 			WithExec([]string{fmt.Sprintf("helm registry login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD %s", option.RegistryUrl)})
 
 		// Push to registry
+		container = container.WithExec([]string{"helm repo update"})
 		container = container.WithExec([]string{fmt.Sprintf("helm push %s-%s.tgz oci://%s/%s", dataChart["name"], option.Version, option.RegistryUrl, option.RepositoryName)})
 	}
 
