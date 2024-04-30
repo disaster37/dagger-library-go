f4eb183e54e943a04ec3703bc83d84fe434d206f
disaster37 <linuxworkgroup@hotmail.com>
2024-04-30 07:56:30 +0000

    fix: helm lint when use private registry as dependency
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- helm/build.go
+++ helm/build.go
@@ -120,8 +120,6 @@ func BuildHelm(ctx context.Context, client *dagger.Client, option *BuildOption) 
 	}
 
 	// package helm
-	container = container.WithExec(helper.ForgeCommand("ls -al /etc/ssl/certs/"))
-	container = container.WithExec(helper.ForgeCommand("exit 2"))
 	container = container.WithExec(helper.ForgeCommand("dependency update"))
 	container = container.WithExec(helper.ForgeCommand("package -u ."))
 
--- helm/lint.go
+++ helm/lint.go
@@ -2,6 +2,9 @@ package helm
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"dagger.io/dagger"
 	"emperror.dev/errors"
@@ -14,6 +17,7 @@ type LintOption struct {
 	WithProxy   bool   `default:"true"`
 	PathContext string `default:"."`
 	WithFiles   map[string]*dagger.File
+	CaPath      string
 }
 
 // Lint permit to lint helm
@@ -27,7 +31,28 @@ func Lint(ctx context.Context, client *dagger.Client, option *LintOption) (err e
 		panic(err)
 	}
 
-	container := getHelmContainer(client, option.PathContext, option.WithProxy).
+	container := getHelmContainer(client, option.PathContext, option.WithProxy)
+
+	if option.CaPath != "" {
+		// Copy the certificate in temporary folder because of the are issue with buildkit when file is symlink
+		caTmpFile, err := os.CreateTemp("", "ca")
+		if err != nil {
+			return errors.Wrap(err, "Error when create temporary file to store CA content")
+		}
+		defer os.Remove(caTmpFile.Name())
+
+		caContent, err := os.ReadFile(option.CaPath)
+		if err != nil {
+			return errors.Wrap(err, "Error when read CA file")
+		}
+		if _, err = caTmpFile.Write(caContent); err != nil {
+			return errors.Wrap(err, "Error when write CA contend")
+		}
+
+		container = container.WithMountedFile(fmt.Sprintf("/etc/ssl/certs/%s", filepath.Base(option.CaPath)), client.Host().File(caTmpFile.Name()))
+	}
+
+	container = container.
 		WithExec(helper.ForgeCommand("dependency update")).
 		WithExec(helper.ForgeCommand("lint ."))
 
