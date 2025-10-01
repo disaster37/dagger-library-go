587d3a3871e451d837d20b9fa75bd60685957ce3
disaster37 <linuxworkgroup@hotmail.com>
2025-10-01 14:46:23 +0000

    fix: fix helm module
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- helm/pipeline.go
+++ helm/pipeline.go
@@ -116,7 +116,7 @@ func (m *Helm) Ci(
 	for _, helmPath := range helmPaths {
 
 		// Init state
-		currentHelmModule := m.WithWorkDir(sourceDirectory).WithSource(rootDir).WithWorkDir(helmPath)
+		currentHelmModule := m.WithSource(rootDir).WithWorkDir(helmPath)
 
 		// Forge target version
 		localVersion := version
